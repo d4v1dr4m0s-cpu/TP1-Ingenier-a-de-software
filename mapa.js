c7a336c08585179bdb4a81b5a8f22a920ad8baa5
NahuVelazquez <nahuel605.nv@gmail.com>
2026-04-17 01:43:41 -0300

    tarjetas telleres y marcadores asociados

--- mapa.js
+++ mapa.js
@@ -8,23 +8,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const talleres = [
-    { nombre: "Taller de guitarra", info: "Lun 19 a 20hs <br> Julio A. Roca", tel: "11-9988-7766", img: "https://www.elvalleonline.com.ar/wp-content/uploads/2024/03/taller-guitarra-trelew-2-e1709642049198.jpeg", redes: "@guitarraIg", coords: [-34.540505, -58.717209] },
-    { nombre: "Taller de carpinteria Artesanal", info: "De lunes a sabados <br> 9 a 16hs <br> Paunero 1848", tel: "11-9988-7766", coords: [-34.539050, -58.714661] },
-    { nombre: "Clases de violin", info: "Miercoles y Jueves <br> 18:30 a 20:30 <br> Tte. General Ricchieri 422", tel: "11-9988-7766", coords: [-34.519531, -58.700979] },
-    { nombre: "Feria de los artesanos", info: "Jueves 10 a 20hs <br> Juan M. Gutierrez 1050", tel: "11-9988-7766", coords: [-34.524944, -58.704787] },
-    { nombre: "Clases de canto", info: "Jueves y Sabados <br> 9 a 11hs <br> La Plata 1724", tel: "11-9988-7766", coords: [-34.521194, -58.705061] },
-    { nombre: "Luthier de guitarra", info: "Lunes a Sabados <br> 8 a 17hs <br> Dorrego 149", tel: "11-9988-7766", coords: [-34.517047, -58.697761] },
-    { nombre: "Clases de dibujo", info: "Miercoles 17 a 20hs <br> Altube 4839", tel: "11-9988-7766", coords: [-34.527989, -58.707886] },
-    { nombre: "Clases de guitarra", info: "Martes 21 a 22hs <br> Concejal acosta 637", tel: "11-9988-7766", coords: [-34.556901, -58.684664] },
-    { nombre: "Clases de canto", info: "Martes y Viernes 20 a 21hs <br> Tte richieri 422", tel: "11-9988-7766", coords: [-34.558174, -58.691956] }
+    { id: 1, nombre: "Taller de guitarra", info: "Lun 19 a 20hs <br> Julio A. Roca", tel: "11-9988-7766", img: "https://www.elvalleonline.com.ar/wp-content/uploads/2024/03/taller-guitarra-trelew-2-e1709642049198.jpeg", redes: "@guitarraIg", coords: [-34.540505, -58.717209] },
+    { id: 2, nombre: "Taller de carpinteria Artesanal", info: "De lunes a sabados <br> 9 a 16hs <br> Paunero 1848", tel: "11-9988-7766", coords: [-34.539050, -58.714661] },
+    { id: 3, nombre: "Clases de violin", info: "Miercoles y Jueves <br> 18:30 a 20:30 <br> Tte. General Ricchieri 422", tel: "11-9988-7766", coords: [-34.519531, -58.700979] },
+    { id: 4, nombre: "Feria de los artesanos", info: "Jueves 10 a 20hs <br> Juan M. Gutierrez 1050", tel: "11-9988-7766", coords: [-34.524944, -58.704787] },
+    { id: 5, nombre: "Clases de canto", info: "Jueves y Sabados <br> 9 a 11hs <br> La Plata 1724", tel: "11-9988-7766", coords: [-34.521194, -58.705061] },
+    { id: 6, nombre: "Luthier de guitarra", info: "Lunes a Sabados <br> 8 a 17hs <br> Dorrego 149", tel: "11-9988-7766", coords: [-34.517047, -58.697761] },
+    { id: 7, nombre: "Clases de dibujo", info: "Miercoles 17 a 20hs <br> Altube 4839", tel: "11-9988-7766", coords: [-34.527989, -58.707886] },
+    { id: 8, nombre: "Clases de guitarra", info: "Martes 21 a 22hs <br> Concejal acosta 637", tel: "11-9988-7766", coords: [-34.556901, -58.684664] },
+    { id: 9, nombre: "Clases de canto", info: "Martes y Viernes 20 a 21hs <br> Tte richieri 422", tel: "11-9988-7766", coords: [-34.558174, -58.691956] }
 ];
 
 var marcadoresEnPantalla = L.layerGroup().addTo(map);
 
-function filtrarTalleres() {
-    var criterio = document.getElementById("barra_buscador").value.toLowerCase();
-    marcadoresEnPantalla.clearLayers();
+let buscador = document.getElementById("barra_buscador"); //criterio a usar en los filtros
 
+function filtrarTalleres(criterio) {
+
+    marcadoresEnPantalla.clearLayers();
     let encontrado = false;
 
     talleres.forEach(function (t) {
@@ -42,6 +43,11 @@ function filtrarTalleres() {
             var m = L.marker(t.coords).bindPopup(contenidoPopup);
             marcadoresEnPantalla.addLayer(m);
 
+            // asociar marcador a tarjeta
+            /*m.on("click", function() {
+                cargarDatosFiltro(t.nombre.toLocaleLowerCase());
+            })*/
+
             // "Centrar en el mapa" si hay coincidencia exacta o es la primera
             if (!encontrado) {
                 map.flyTo(t.coords, 16); // Hace el efecto de zoom suave
@@ -52,7 +58,13 @@ function filtrarTalleres() {
 }
 
 
-document.getElementById("btnBuscar").onclick = filtrarTalleres;
+//document.getElementById("btnBuscar").onclick = filtrarTalleres(buscador.value);
+
+
+//muestra los marcadores cuando apretas buscar
+document.getElementById("btnBuscar").onclick = function () {
+    filtrarTalleres(buscador.value);
+};
 
 
 function cargarDatosFiltro(filtro) {
@@ -63,29 +75,38 @@ function cargarDatosFiltro(filtro) {
     talleres.forEach(taller => {
 
         if (taller.nombre.toLowerCase().includes(filtro.toLowerCase())) {
-        
+
             const item = document.createElement("li");
 
-        item.innerHTML = `
+            item.innerHTML = `
         <div class="tarjeta">
         <h2>${taller.nombre}</h2>
         <p>${taller.info}</p>
         </div>
         `;
+            // obtengo tarjeta creada
+            const tarjeta = item.querySelector(".tarjeta");
 
-        lista.appendChild(item);
+            // agrego evento click
+            tarjeta.addEventListener("click", function () {
+                // asocio tarjeta a marcador
+                filtrarTalleres(taller.nombre.toLocaleLowerCase());
+            });
+
+            lista.appendChild(item);
         }
-
     });
-
 }
-
-const buscador = document.getElementById("barra_buscador");
 
 buscador.addEventListener("input", () => {
     cargarDatosFiltro(buscador.value);
+
+    //muestra los marcadores antes de apretar buscar(a medida que escribis)    
+    //    filtrarTalleres(buscador.value);
 })
 
-cargarDatosFiltro(""); //por defecto muestro todo
+//por defecto muestro todo
+filtrarTalleres("");
+cargarDatosFiltro("");
 
 
