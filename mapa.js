a1d98a7437d19cba7c73186c9b94f0e1a20931ec
NahuVelazquez <nahuel605.nv@gmail.com>
2026-04-20 05:37:07 -0300

    linkeo marcador-tarjeta y expansion tarjeta

--- mapa.js
+++ mapa.js
@@ -8,15 +8,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const talleres = [
-    { id: 1, nombre: "Taller de guitarra", info: "Lun 19 a 20hs <br> Julio A. Roca", tel: "11-9988-7766", img: "https://www.elvalleonline.com.ar/wp-content/uploads/2024/03/taller-guitarra-trelew-2-e1709642049198.jpeg", redes: "@guitarraIg", coords: [-34.540505, -58.717209] },
-    { id: 2, nombre: "Taller de carpinteria Artesanal", info: "De lunes a sabados <br> 9 a 16hs <br> Paunero 1848", tel: "11-9988-7766", coords: [-34.539050, -58.714661] },
-    { id: 3, nombre: "Clases de violin", info: "Miercoles y Jueves <br> 18:30 a 20:30 <br> Tte. General Ricchieri 422", tel: "11-9988-7766", coords: [-34.519531, -58.700979] },
-    { id: 4, nombre: "Feria de los artesanos", info: "Jueves 10 a 20hs <br> Juan M. Gutierrez 1050", tel: "11-9988-7766", coords: [-34.524944, -58.704787] },
-    { id: 5, nombre: "Clases de canto", info: "Jueves y Sabados <br> 9 a 11hs <br> La Plata 1724", tel: "11-9988-7766", coords: [-34.521194, -58.705061] },
-    { id: 6, nombre: "Luthier de guitarra", info: "Lunes a Sabados <br> 8 a 17hs <br> Dorrego 149", tel: "11-9988-7766", coords: [-34.517047, -58.697761] },
-    { id: 7, nombre: "Clases de dibujo", info: "Miercoles 17 a 20hs <br> Altube 4839", tel: "11-9988-7766", coords: [-34.527989, -58.707886] },
-    { id: 8, nombre: "Clases de guitarra", info: "Martes 21 a 22hs <br> Concejal acosta 637", tel: "11-9988-7766", coords: [-34.556901, -58.684664] },
-    { id: 9, nombre: "Clases de canto", info: "Martes y Viernes 20 a 21hs <br> Tte richieri 422", tel: "11-9988-7766", coords: [-34.558174, -58.691956] }
+    { id: 1, nombre: "Taller de guitarra", horario: "Lun 19 a 20hs", direccion: "Julio A. Roca 2561", tel: "11-9988-7766", img: "https://www.elvalleonline.com.ar/wp-content/uploads/2024/03/taller-guitarra-trelew-2-e1709642049198.jpeg", redes: "@guitarraIg", coords: [-34.540505, -58.717209] },
+    { id: 2, nombre: "Taller de carpinteria Artesanal", horario: "De lunes a sabados <br> 9 a 16hs", direccion: "Paunero 1848", tel: "11-9988-7766", img:"https://www.gaiarestauracion.com/imagenes/r_mueble/curso_ebanisteria2.jpg", redes: "@CarpArt",coords: [-34.539050, -58.714661] },
+    { id: 3, nombre: "Clases de violin", horario: "Miercoles y Jueves <br> 18:30 a 20:30", direccion: "Tte. General Ricchieri 422", tel: "11-9988-7766", img: "https://cbblanca-bue.infd.edu.ar/sitio/wp-content/uploads/2025/05/CLASE-DE-INSTRUMENTO.jpg", redes: "@Clavio_ig",coords: [-34.519531, -58.700979] },
+    { id: 4, nombre: "Feria de los artesanos", horario: "Jueves 10 a 20hs", direccion: "Juan M. Gutierrez 1050", tel: "11-9988-7766", img:"https://www.olavarria.gov.ar/wp-content/uploads/2020/12/Cultura-Feria-artesanos-1-1024x682.jpg", redes: "@FeriART",coords: [-34.522531471037794, -58.70906656302671] },
+    { id: 5, nombre: "Clases de canto", horario: "Jueves y Sabados <br> 9 a 11hs", direccion: "La Plata 1724", tel: "11-9988-7766", img:"https://elizabethdolinska.com/wp-content/uploads/2020/05/clases-de-canto-en-madrid-088.jpg", redes: "@clanto",coords: [-34.56344123055479, -58.6865394296436] },
+    { id: 6, nombre: "Luthier de guitarra", horario: "Lunes a Sabados <br> 8 a 17hs", direccion: "Dorrego 149", tel: "11-9988-7766", img:"https://www.guitarrasfuentes.com/wp-content/uploads/2021/12/que-es-un-luthier-de-guitarras-1536x1225.jpg", redes: "@Lutharra",coords: [-34.54353165038189, -58.68962795652001] },
+    { id: 7, nombre: "Clases de dibujo", horario: "Miercoles 17 a 20hs", direccion: "Altube 4839", tel: "11-9988-7766",  img:"https://mencantapintar.com/wp-content/uploads/2024/12/WhatsApp-Image-2024-11-21-at-18.49.18-1-1024x771.jpeg",redes: "@dibuses",coords: [-34.527989, -58.707886] },
+    { id: 8, nombre: "Clases de guitarra", horario: "Martes 21 a 22hs", direccion: "Concejal acosta 637", tel: "11-9988-7766", img:"https://www.guitarraguadalajara.com/wp-content/uploads/2020/02/228056019-md-1536x1025.jpeg",redes: "@guitana",coords: [-34.556901, -58.684664] },
+    { id: 9, nombre: "Clases de canto", horario: "Martes y Viernes 20 a 21hs", direccion: "Tte richieri 422", tel: "11-9988-7766", img:"https://elizabethdolinska.com/wp-content/uploads/2018/09/clases-de-canto-en-madrid-025.jpg", redes: "@Cantina",coords: [-34.550582502068444, -58.6969370517178] }
 ];
 
 var marcadoresEnPantalla = L.layerGroup().addTo(map);
@@ -39,13 +39,18 @@ function crearMarcadores(listaFiltrada) {
                 <div style="text-align:center;">
                     <img src="${t.img}" style="width:100%; border-radius:5px;"><br>
                     <strong style="font-size:16px;">${t.nombre}</strong><br>
-                    <p>🕒 ${t.info}<br>
+                    <p> 
+                    ${t.direccion}<br>
+                    🕒 ${t.horario}<br>
                     📞 ${t.tel}<br>
-                    📱 ${t.redes}</p>
+                    📱 ${t.redes}
+                    </p>
                 </div>
             `;
 
         var m = L.marker(t.coords).bindPopup(contenidoPopup);
+        m._id = t.id;
+
         marcadoresEnPantalla.addLayer(m);
 
         // "Centrar en el mapa" si hay coincidencia exacta o es la primera
@@ -53,6 +58,21 @@ function crearMarcadores(listaFiltrada) {
             map.flyTo(t.coords, 16); // Hace el efecto de zoom suave
             encontrado = true;
         }
+
+        //capturo evento marcador
+        m.on("click", () => {
+           document.querySelectorAll(".tarjeta").forEach(t => {
+                t.classList.remove("expandida");
+                t.classList.remove("resaltada");
+            });
+            const tarjeta = document.querySelector(`.tarjeta[data-id='${t.id}']`);
+            if (tarjeta) {
+                tarjeta.classList.add("expandida");
+                tarjeta.classList.add("resaltada");
+                
+                tarjeta.scrollIntoView({ behavior: "smooth", block: "center" });
+            }
+        });
 
     });
 }
@@ -68,14 +88,41 @@ function crearTarjetas(listaFiltrada) {
         const item = document.createElement("li");
 
         item.innerHTML = `
-        <div class="tarjeta">
+        <div class="tarjeta" data-id="${taller.id}">
         <h2>${taller.nombre}</h2>
-        <p>${taller.info}</p>
+            <div class="detalle">
+            <p>
+            <img src="${taller.img}" style="width:100%; border-radius:5px;"><br>
+            Horario: ${taller.horario} <br>
+            Tel: ${taller.tel} <br>
+            Redes: ${taller.redes} <br>
+            </p>
+            </div>
+        <p> Dirección: ${taller.direccion}</p>      
         </div>
         `;
 
         lista.appendChild(item);
 
+        //capturo evento de tarjeta
+        const tarjeta = item.querySelector(".tarjeta");
+        tarjeta.addEventListener("click", () => {
+
+            //limpio las tarjetas, vuelven a su estado base
+            document.querySelectorAll(".tarjeta").forEach(t => {
+                t.classList.remove("expandida");
+                t.classList.remove("resaltada");
+            });
+            tarjeta.classList.add("expandida");
+            tarjeta.classList.add("resaltada");
+
+            marcadoresEnPantalla.eachLayer(m => {
+                if (m._id === taller.id) {
+                    m.openPopup();
+                    map.flyTo(m.getLatLng(), 16);
+                }
+            });
+        });
     });
 }
 
